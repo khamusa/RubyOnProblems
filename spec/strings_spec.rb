3abc61e029ed68d347f4142dd44aa7aaa1704a48
Samuel Brandão <gb.samuel@gmail.com>
2014-04-12 19:02:38 +0200

    New version for palindromes, using scan method for strings

--- spec/strings_spec.rb
+++ spec/strings_spec.rb
@@ -1,27 +1,41 @@
 # bowling_spec.rb
 require_relative '../src/01-strings'
 
+shared_examples "palindrome_examples" do |proc|
+	it "returns true for simple palindrome strings" do
+		expect(proc.call("aabaa")).to be_true
+		expect(proc.call("ana")).to be_true
+		expect(proc.call("anna")).to be_true
+		expect(proc.call("lkjjkllkjjkl")).to be_true
+	end
+
+	it "returns true for palindrome strings with different case" do
+		expect(proc.call("aabAa")).to be_true
+		expect(proc.call("anA")).to be_true
+		expect(proc.call("anNA")).to be_true
+		expect(proc.call("lKjJkllkjJkl")).to be_true
+	end
+
+	it "returns true for palindrome strings ignoring punctuation" do
+		expect(proc.call("aab.Aa")).to be_true
+		expect(proc.call("a1,n1A")).to be_true
+		expect(proc.call("Rise to vote, sir!")).to be_true
+		expect(proc.call("so cool! looc os!")).to be_true
+	end
+
+	it "returns false for non palindrome strings" do
+		expect(proc.call("aabba")).to be_false
+		expect(proc.call("anaa")).to be_false
+		expect(proc.call("a1nn2a")).to be_false
+		expect(proc.call("ljjkllkjjkl")).to be_false
+		expect(proc.call("Rise not to vote, sir!")).to be_false
+	end
+end
 describe String do
 	describe 'is_palindrome?' do 
-		it "returns true for simple palindrome strings" do
-			expect("aabaa".is_palindrome?).to be_true
-			expect("ana".is_palindrome?).to be_true
-			expect("anna".is_palindrome?).to be_true
-			expect("lkjjkllkjjkl".is_palindrome?).to be_true
-		end
-
-		it "returns true for palindrome strings with different case" do
-			expect("aabAa".is_palindrome?).to be_true
-			expect("anA".is_palindrome?).to be_true
-			expect("anNA".is_palindrome?).to be_true
-			expect("lKjJkllkjJkl".is_palindrome?).to be_true
-		end
-
-		it "returns true for palindrome strings ignoring punctuation" do
-			expect("aab.Aa".is_palindrome?).to be_true
-			expect("a,nA".is_palindrome?).to be_true
-			expect("Rise to vote, sir!".is_palindrome?).to be_true
-			expect("so cool! looc os!".is_palindrome?).to be_true
-		end
+		include_examples "palindrome_examples", Proc.new { |string| string.is_palindrome? }
+	end
+	describe 'is_palindrome_2?' do 
+		include_examples "palindrome_examples", Proc.new { |string| string.is_palindrome_2? }
 	end
 end
--- src/01-strings.rb
+++ src/01-strings.rb
@@ -1,7 +1,12 @@
 class String
 	# determine if a string is a palindrome
 	def is_palindrome?
-		pre = self.downcase.gsub(/[^a-zA-Z]/, '')
+		pre = self.downcase.gsub(/[^a-zA-Z0-9]/, '')
 		pre.reverse == pre
+	end
+
+	def is_palindrome_2?
+		letters = self.downcase.scan(/\w/) 	# returns an array of characters
+    	letters == letters.reverse			# this reverse is called on an array
 	end
 end
